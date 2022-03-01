d803ed2f4bdd92bf71d584e40e9174868933f5bc
Vombato <luca.vombato@gmail.com>
2022-03-01 16:13:07 +0100

    Pylint fix

--- mlgen.py
+++ mlgen.py
@@ -1,13 +1,15 @@
+# pylint: disable=missing-module-docstring
 import random
 
-def getMLPhrase(wordlist):
+
+def get_ml_phrase(wordlist):
     msg = ""
     cong = ["e", "o", "se", "ma", "in", "da", "per", "su"]
-    for n in range(random.randint(3, 10)):
+    for num in range(random.randint(3, 10)):
         sub = random.choice(wordlist)
         ind = random.randint(1, 7)
-        if n % ind == 0:
-            if n == 0:
+        if num % ind == 0:
+            if num == 0:
                 msg += random.choice(cong)
             else:
                 msg += " " + random.choice(cong)
@@ -18,10 +20,10 @@ def getMLPhrase(wordlist):
 
 
 def main():
-    my_file = open("parole.txt", "r")
-    data = my_file.read()
-    list = data.split("\n")
-    print(getMLPhrase(list))
+    with open("parole.txt", "r", encoding="utf-8") as my_file:
+        data = my_file.read()
+        word_list = data.split("\n")
+        print(get_ml_phrase(word_list))
 
 
 if __name__ == "__main__":
