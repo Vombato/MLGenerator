5dc71f36e6a20296d479001655fdd749175eba49
Luca Bandini <luca.vombato@gmail.com>
2022-03-01 16:01:21 +0100

    Changed congiunzioni randomness

--- mlgen.py
+++ mlgen.py
@@ -1,26 +1,28 @@
 import random
 
 def getMLPhrase(wordlist):
-	msg = ""
-	cong = ["e", "o", "se", "ma", "in"]
-	for n in range(random.randint(3, 10)):
-		sub=random.choice(wordlist)
-		if n % 4 == 0:
-			if n == 0:
-				msg+=random.choice(cong)
-			else:
-				msg+=" "+random.choice(cong)
-		else:
-			msg+=" "+sub
-	msg+="."
-	return msg.capitalize()
+    msg = ""
+    cong = ["e", "o", "se", "ma", "in", "da", "per", "su"]
+    for n in range(random.randint(3, 10)):
+        sub = random.choice(wordlist)
+        ind = random.randint(1, 7)
+        if n % ind == 0:
+            if n == 0:
+                msg += random.choice(cong)
+            else:
+                msg += " " + random.choice(cong)
+        else:
+            msg += " " + sub
+    msg += "."
+    return msg.capitalize()
+
 
 def main():
-	my_file = open("parole.txt", "r")
-	data = my_file.read()
-	list = data.split("\n")
-	print(getMLPhrase(list))
-	
+    my_file = open("parole.txt", "r")
+    data = my_file.read()
+    list = data.split("\n")
+    print(getMLPhrase(list))
+
 
 if __name__ == "__main__":
-	main()
+    main()
